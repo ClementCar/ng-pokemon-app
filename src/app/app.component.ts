ef88e4035b7237105ecbb4e01fc6848cf8988bb1
Clement Carcenac <clementcarcenac@gmail.com>
2023-02-09 15:35:27 +0100

    templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,28 @@ import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Liste de Pokémons !</h1>`,
+  templateUrl:'app.component.html',
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   constructor() {}
 
   ngOnInit(): void {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0])
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if (pokemon) {
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon
+    } else {
+      console.log(`existe ap`);
+      this.pokemonSelected = pokemon
+    }
+    
+    // this.pokemonSelected = 
   }
 }
